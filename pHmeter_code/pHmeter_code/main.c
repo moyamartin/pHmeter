9b8c560a4ee4da5b94450647d2871768f9a0bf75
moyamartin <moyamartin1@gmail.com>
2017-11-30 00:48:01 -0300

    LCD - ADC Working - updated PCB

--- pHmeter_code/pHmeter_code/main.c
+++ pHmeter_code/pHmeter_code/main.c
@@ -9,30 +9,67 @@
  #define F_CPU 16000000UL // 16 MHz Clock Speed
  #endif
 
+
 #include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lcd.h"
 
 void init_adc();
-uint16_t adc_read(uint8_t channel);
+void adc_single_read(uint16_t * value);
+double ph_avg();
+void ftoa(double value, char * buffer);
 
 int main(void)
-{
-	DDRB |= 1<<DDB1;
-	uint16_t resultado = 0;
-    init_adc();
+{	
+	double ph_result = 0.0;
+	char buffer[5];
+	DDRD = 0xFF;
+	DDRB = 0xFF;
+	Lcd8_Init();
+	init_adc();
     while (1) 
     {
-		resultado = adc_read(1);
-		if (resultado > 512){
-			PORTB |= 1<<DDB1;
-		}
-		else
-		{
-			PORTB &= ~(1<<DDB1);
-		}
+		Lcd8_Set_Cursor(1,0);
+		Lcd8_Write_String("pH-Meter V1.0");
+		Lcd8_Set_Cursor(2,0);
+		ph_result = ph_avg();
+		ftoa(ph_result, buffer);
+		Lcd8_Write_String(buffer);
+		_delay_ms(10);
     }
 }
+
+void ftoa(double value, char * buffer)
+{
+	int aux_1 = 0, aux_2 = 0;
+	char buffer_1[2], buffer_2[2];
+	if(value < 10)
+	{
+		strcpy(buffer, "0");
+	}
+	aux_1 = (int) value;
+	itoa(aux_1, buffer_1, 10);
+	itoa(aux_2, buffer_2, 10);
+	//strcat(buffer, buffer_1);
+	strcat(buffer, ",");
+	strcat(buffer, buffer_2);
+
+}
+
+
+
+void init_adc()
+{
+	ADCSRA |= (1<<ADPS2|1<<ADPS1|1<<ADPS0);		//set pre-escaler to 128 -> 125KHz ADC_clock
+	ADCSRA |= 1<<ADEN;							//Enables ADC
+	ADMUX |= (1<<REFS0 | 1<<REFS1);				//Enables 1V1 Vref
+}
+
 /*
-ADC Initialization
+ADC Channels
 0: ADC 0
 1: ADC 1
 2: ADC 2
@@ -42,19 +79,26 @@ ADC Initialization
 6: ADC 6
 7: ADC 7
 8: Temperature sensor
-14: 1.1V 
+14: 1.1V
 15: GND
 */
-void init_adc()
+void adc_single_read(uint16_t * result)
 {
-	ADCSRA |= (1<<ADPS2|1<<ADPS1|1<<ADPS0);		//set pre-escaler to 128 -> 125KHz ADC_clock
-	ADCSRA |= 1<<ADEN;							//Enables ADC
+	ADMUX |= MUX0;
+	ADCSRA |= 1<<ADSC;
+	while((ADCSRA & 1<<ADSC)){};		
+	*result = ADCW;
 }
 
-uint16_t adc_read(uint8_t channel)
+double ph_avg()
 {
-	ADMUX = channel;
-	ADCSRA |= 1<<ADSC;
-	while((ADCSRA & 1<<ADSC)){};
-	return ADCW;
+	uint16_t adc_value = 0, acum = 0;
+	for(int i = 0; i<50; i++)
+	{
+		adc_single_read(&adc_value);
+		acum += adc_value;
+	}
+	return (acum/50.0)*(14.0/1023);
 }
+
+
